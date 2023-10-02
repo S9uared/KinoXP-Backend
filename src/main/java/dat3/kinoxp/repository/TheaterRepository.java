a106fc61c1b6ee6fc2b313fa2ea0444d24b9d67c
simo75m1 <simo75m1@stud.kea.dk>
2023-10-02 12:53:33 +0200

    KIN-29 Entity & Repo done

---
+++ src/main/java/dat3/kinoxp/repository/TheaterRepository.java
@@ -0,0 +1,7 @@
+package dat3.kinoxp.repository;
+
+import dat3.kinoxp.entity.Theater;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TheaterRepository extends JpaRepository<Theater, Integer> {
+}
