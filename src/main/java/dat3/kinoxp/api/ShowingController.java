0dbde0990caac53d1d61bc842f5c7a22779222b0
laura ramgil <lauraramgil@hotmail.com>
2023-10-04 10:00:17 +0200

    Implement ShowinController

--- src/main/java/dat3/kinoxp/api/ShowingController.java
+++ src/main/java/dat3/kinoxp/api/ShowingController.java
@@ -37,6 +37,7 @@ public class ShowingController {
 
     @GetMapping(path = "/{id}")
     ShowingResponse getShowingById(@PathVariable int id){
+
         return showingService.findById(id);
     }
 
