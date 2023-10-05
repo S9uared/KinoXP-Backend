353f32f209830f61b52df3274e6c9e1a0713c41c
G1tHubberen <frej3215@stud.kea.dk>
2023-10-05 14:51:00 +0200

    CustomerInfo and CustomerInfoRepository

---
+++ src/main/java/dat3/kinoxp/entity/CustomerInfo.java
@@ -0,0 +1,15 @@
+package dat3.kinoxp.entity;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+
+@Entity
+public class CustomerInfo {
+
+}
