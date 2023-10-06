95b21b73a5f44aae49190cfe39744ab7e26e4365
G1tHubberen <frej3215@stud.kea.dk>
2023-10-06 09:24:05 +0200

    CustomerInfo and CustomerInfoRepository

--- src/main/java/dat3/kinoxp/repository/CustomerInfoRepository.java
+++ src/main/java/dat3/kinoxp/repository/CustomerInfoRepository.java
@@ -2,7 +2,6 @@ package dat3.kinoxp.repository;
 
 import dat3.kinoxp.entity.CustomerInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public class CustomerInfoRepository extends JpaRepository<CustomerInfo, String> {
 }
