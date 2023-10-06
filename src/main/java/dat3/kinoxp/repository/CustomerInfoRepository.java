625830a7c64ba64e73e0dc49830d70376352bf81
G1tHubberen <frej3215@stud.kea.dk>
2023-10-06 10:09:37 +0200

    CustomerInfo edits and updates

--- src/main/java/dat3/kinoxp/repository/CustomerInfoRepository.java
+++ src/main/java/dat3/kinoxp/repository/CustomerInfoRepository.java
@@ -3,5 +3,5 @@ package dat3.kinoxp.repository;
 import dat3.kinoxp.entity.CustomerInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class CustomerInfoRepository extends JpaRepository<CustomerInfo, String> {
+public interface CustomerInfoRepository extends JpaRepository<CustomerInfo, String> {
 }
