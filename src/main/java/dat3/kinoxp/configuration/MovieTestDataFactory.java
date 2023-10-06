a044599ce05aa8c9799fda1d6765f4e14e2e8760
Aileena <aile0074@stud.kea.dk>
2023-10-06 10:10:08 +0200

    Add new movie system

--- src/main/java/dat3/kinoxp/configuration/MovieTestDataFactory.java
+++ src/main/java/dat3/kinoxp/configuration/MovieTestDataFactory.java
@@ -1,6 +1,7 @@
 package dat3.kinoxp.configuration;
 
 import dat3.kinoxp.entity.Movie;
+import dat3.kinoxp.dto.MovieOmdbRequest;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,25 +9,32 @@ import java.util.List;
 import java.util.Random;
 
 public class MovieTestDataFactory {
-    private static final List<String> movieNames = Arrays.asList(
+    private static final List<String> movieTitles = Arrays.asList(
             "Movie1", "Movie2", "Movie3", "Movie4", "Movie5",
             "Movie6", "Movie7", "Movie8", "Movie9", "Movie10"
     );
 
-    private static final List<Integer> ageRestrictions = Arrays.asList(0, 6, 12, 16, 18);
+    private static final List<Integer> years = Arrays.asList(2000, 2005, 2010, 2015, 2020);
 
-    private static final List<String> categories = Arrays.asList(
+    private static final List<String> genres = Arrays.asList(
             "Action", "Comedy", "Drama", "Science Fiction", "Horror"
     );
 
-    public static List<Movie> generateTestMovies() {
+    public static List<MovieOmdbRequest> generateTestMovies() {
         Random random = new Random();
-        List<Movie> movies = new ArrayList<>();
+        List<MovieOmdbRequest> movies = new ArrayList<>();
         for (int i = 0; i < 50; i++) {
-            String movieName = movieNames.get(random.nextInt(movieNames.size()));
-            int ageRestriction = ageRestrictions.get(random.nextInt(ageRestrictions.size()));
-            String category = categories.get(random.nextInt(categories.size()));
-            Movie movie = new Movie(movieName, ageRestriction, category);
+            String title = movieTitles.get(random.nextInt(movieTitles.size()));
+            String year = String.valueOf(years.get(random.nextInt(years.size())));
+            String genre = genres.get(random.nextInt(genres.size()));
+
+            MovieOmdbRequest movie = new MovieOmdbRequest();
+            movie.setTitle(title);
+            movie.setYear(year);
+            movie.setGenre(genre);
+
+            // Set other fields of the MovieOmdbRequest as needed
+
             movies.add(movie);
         }
         return movies;
--- src/main/java/dat3/kinoxp/service/MovieService.java
+++ src/main/java/dat3/kinoxp/service/MovieService.java
@@ -1,20 +1,73 @@
 package dat3.kinoxp.service;
 
-import dat3.kinoxp.dto.MovieRequest;
-import dat3.kinoxp.dto.MovieResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import dat3.kinoxp.api_facade.AzureTranslate;
+import dat3.kinoxp.api_facade.OmdbFacade;
+import dat3.kinoxp.dto.MovieOmdbResponse;
 import dat3.kinoxp.entity.Movie;
 import dat3.kinoxp.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class MovieService {
 
+    MovieRepository movieRepository;
+
+    @Autowired
+    AzureTranslate translator;
+
+    @Autowired
+    OmdbFacade omdbFacade;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public Movie getMovieByImdbId(String imdbId) {
+        return movieRepository.findByImdbID(imdbId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Movie not found"));
+    }
+
+
+    public Movie addMovie(String imdbId) throws JsonProcessingException {
+        MovieOmdbResponse dto = omdbFacade.getMovie(imdbId);
+        //String dkPlot = translator.translate(dto.getPlot());
+
+        Movie movie = Movie.builder()
+                .title(dto.getTitle())
+                .year(dto.getYear())
+                .rated(dto.getRated())
+                .released(dto.getReleased())
+                .runtime(dto.getRuntime())
+                .genre(dto.getGenre())
+                .director(dto.getDirector())
+                .writer(dto.getWriter())
+                .actors(dto.getActors())
+                .metascore(dto.getMetascore())
+                .imdbRating(dto.getImdbRating())
+                .imdbVotes(dto.getImdbVotes())
+                .website(dto.getWebsite())
+                .response(dto.getResponse())
+                .plot(dto.getPlot())
+                //.plotDK(dkPlot)
+                .poster(dto.getPoster())
+                .imdbID(dto.getImdbID())
+                .build();
+        try {
+            movieRepository.save(movie);
+            return movie;
+        } catch (DataIntegrityViolationException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getRootCause().getMessage());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not add movie");
+        }
+    }
+}
+
+    /*
     private final MovieRepository movieRepository;
 
     public MovieService(MovieRepository movieRepository) {
@@ -66,3 +119,4 @@ public class MovieService {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Movie with this id does not exist"));
     }
 }
+*/
