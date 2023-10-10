304f05c4c881bd7e1affa17ddf7c3cf4ea9aac9f
laura ramgil <lauraramgil@hotmail.com>
2023-10-10 10:51:41 +0200

    Changed getShowing to order by date and time

--- src/main/java/dat3/kinoxp/repository/ShowingRepository.java
+++ src/main/java/dat3/kinoxp/repository/ShowingRepository.java
@@ -1,7 +1,9 @@
 package dat3.kinoxp.repository;
 
 import dat3.kinoxp.entity.Showing;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -15,4 +17,7 @@ public interface ShowingRepository extends JpaRepository<Showing, Integer> {
     List<Showing> getShowingsByMovieId(int movieId);
     List<Showing> getShowingsByMovieGenre(String genre);
     boolean existsByTheaterId(int id);
+
+    @Query("SELECT s FROM Showing s ORDER BY s.date ASC, s.time ASC")
+    List<Showing> findAllOrderByDateAndTime();
 }
