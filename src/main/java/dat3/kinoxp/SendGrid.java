3259e5cdebc7b5631b373ac07d6e342036498274
G1tHubberen <frej3215@stud.kea.dk>
2023-10-12 12:08:59 +0200

    Send email to customers

--- src/main/java/dat3/kinoxp/SendGrid.java
+++ src/main/java/dat3/kinoxp/SendGrid.java
@@ -1,15 +1,26 @@
 package dat3.kinoxp;
 import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.*;
+
 import java.io.IOException;
 
 public class SendGrid {
+
     public static void main(String[] args) throws IOException {
+        Email from = new Email("test@example.com");
+        String subject = "Sending with Twilio SendGrid is Fun";
+        Email to = new Email("test@example.com");
+        Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
+        Mail mail = new Mail(from, subject, to, content);
+
+        String sendgridApiKey = System.getenv("SENDGRID_API_KEY");
+        SendGrid sg = new SendGrid(sendgridApiKey);
+        Request request = new Request();
         try {
-            SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-            Request request = new Request();
             request.setMethod(Method.POST);
             request.setEndpoint("mail/send");
-            request.setBody("{\"personalizations\":[{\"to\":[{\"email\":\"test@example.com\"}],\"subject\":\"Sending with Twilio SendGrid is Fun\"}],\"from\":{\"email\":\"test@example.com\"},\"content\":[{\"type\":\"text/plain\",\"value\": \"and easy to do anywhere, even with Java\"}]}");
+            request.setBody(mail.build());
             Response response = sg.api(request);
             System.out.println(response.getStatusCode());
             System.out.println(response.getBody());
