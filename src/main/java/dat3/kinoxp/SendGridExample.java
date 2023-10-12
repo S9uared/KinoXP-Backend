0db35dd34a7a64c16f6574fb2c56accdd01ba8e4
G1tHubberen <frej3215@stud.kea.dk>
2023-10-12 14:15:38 +0200

    Send email to customers

--- src/main/java/dat3/kinoxp/SendGridExample.java
+++ src/main/java/dat3/kinoxp/SendGridExample.java
@@ -2,21 +2,22 @@ package dat3.kinoxp;
 import com.sendgrid.*;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.*;
+import com.sendgrid.SendGrid;
 
 import java.io.IOException;
 
-public class SendGrid {
+public class SendGridExample {
 
     public static void main(String[] args) throws IOException {
-        Email from = new Email("test@example.com");
+        Email from = new Email("frejajep@hotmail.com");
         String subject = "Sending with Twilio SendGrid is Fun";
-        Email to = new Email("test@example.com");
+        Email to = new Email("frejajep2002@hotmail.com");
         Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
         Mail mail = new Mail(from, subject, to, content);
 
-        String sendgridApiKey = System.getenv("SENDGRID_API_KEY");
-        SendGrid sg = new SendGrid(sendgridApiKey);
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
         Request request = new Request();
+
         try {
             request.setMethod(Method.POST);
             request.setEndpoint("mail/send");
@@ -27,6 +28,15 @@ public class SendGrid {
             System.out.println(response.getHeaders());
         } catch (IOException ex) {
             throw ex;
+        }
+
+        SendGrid sendGrid = null;
+        Response response = sendGrid.api(request);
+
+        if (response.getStatusCode() == 202) {
+            System.out.println("Email sent successfully!");
+        } else {
+            System.out.println("Failed to send the email. Status code: " + response.getStatusCode());
         }
     }
 }
