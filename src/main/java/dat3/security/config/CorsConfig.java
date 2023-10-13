3e7ba55ee081ea3c0220795ccb3d5bdfd260fdfa
laura ramgil <lauraramgil@hotmail.com>
2023-10-13 10:21:51 +0200

    cors setup

--- src/main/java/dat3/security/config/CorsConfig.java
+++ src/main/java/dat3/security/config/CorsConfig.java
@@ -12,8 +12,7 @@ public class CorsConfig {
 
     //When you know the domain name for your client, add it below before deployment
     private static final String[] ALLOWED_ORIGINS = new String[] {
-            "*",
-            "https://example.com/",
+            "https://mango-bush-0b7fdb803.3.azurestaticapps.net",
             "http://127.0.0.1:5500"
     };
     @Bean
